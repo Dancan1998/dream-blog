729dc9d3268d9676bfafbbc98655b3388af0276b
Dancan Chibole <dancankingstar@gmail.com>
2020-12-11 09:02:59 -0800

    responsive navbar using reactjs

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,29 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 
 function Header() {
+  const [toggle, setToggle] = useState(false);
+  const handleToggle = () => {
+    return setToggle(!toggle);
+  };
   return (
-    <header className="header">
-      <nav className="headerMenu">
-        <a href="#">Home</a>
-        <a href="#">About us</a>
-        <a href="#">Contact Us</a>
-      </nav>
-      <div className="socialMedia">
-        <a href="#" className="margiN">
-          <i class="fab fa-facebook-f"></i>
-        </a>
-        <a href="#" className="margiN">
-          <i class="fab fa-twitter"></i>
-        </a>
-        <a href="#" className="margiN">
-          <i class="fab fa-instagram"></i>
-        </a>
-        <a href="#">
-          <i class="fab fa-linkedin-in"></i>
-        </a>
+    <section className="header-container">
+      <div className="menu-icon" onClick={handleToggle}>
+        <i className={toggle ? "fas fa-times" : "fas fa-bars"}></i>
       </div>
-    </header>
+      <header className={toggle ? "header active" : "header"}>
+        <nav className="headerMenu">
+          <a href="#">Home</a>
+          <a href="#">About us</a>
+          <a href="#">Contact Us</a>
+        </nav>
+        <div className="socialMedia">
+          <a href="#" className="margiN">
+            <i class="fab fa-facebook-f"></i>
+          </a>
+          <a href="#" className="margiN">
+            <i class="fab fa-twitter"></i>
+          </a>
+          <a href="#" className="margiN">
+            <i class="fab fa-instagram"></i>
+          </a>
+          <a href="#">
+            <i class="fab fa-linkedin-in"></i>
+          </a>
+        </div>
+      </header>
+    </section>
   );
 }
 
